0dfb4010e16da4f07a389146f18118d14011e79f
mrams07 <amansakdasariya@gmail.com>
2024-06-05 15:05:39 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,91 +5,58 @@ export default function TextForm({ heading, mode, showAlert, btnColor }) {
     var pascal
     //Firsr case
     const handleFirstClick = () => {
-        if (text.length > 0) {
             const transformed = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
             setText(transformed);
             showAlert("Converted to First case",'Success');
-        }
-        else{
-            showAlert("Enter some text first", "Error")
-        }
     };
     // Uppercase
     const handleUpClick = () => {
-        if(text.length > 0){
         setText(text.toUpperCase());
-        showAlert("Converted to Uppercase",'Success');}
-        else{
-            showAlert("Enter some text first", "Error")
-        }
+        showAlert("Converted to Uppercase",'Success');
     };
 
     // Lowercase
     const handleLoClick = () => {
-         if (text.length > 0) {
         setText(text.toLowerCase());
-        showAlert("Converted to Lowercase",'Success');}
-        else{
-            showAlert("Enter some text first", "Error")
-        }
+        showAlert("Converted to Lowercase",'Success');
     };
 
     // PascalCase
     const handlePascalClick = () => {
-         if (text.length > 0) {
         pascal = text.split(' ')
         pascal = pascal.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
         pascal = pascal.join('');
         setText(pascal)
         pascal = ''
-        showAlert("Converted to Pascal Case",'Success');}
-        else{
-            showAlert("Enter some text first", "Error")
-        }
+        showAlert("Converted to Pascal Case",'Success');
     };
 
     // CamellCase
     const handleCamellClick = () => {
-         if (text.length > 0) {
         pascal = text.split(' ')
         pascal = pascal.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
         pascal = pascal.join('');
         pascal = pascal.charAt(0).toLowerCase() + pascal.slice(1);
         setText(pascal)
         pascal = ''
-        showAlert("Converted to Camell Case",'Success');}
-        else{
-            showAlert("Enter some text first", "Error")
-        }
+        showAlert("Converted to Camell Case",'Success');
     }
     //Extra Spaces
     const handleExtraSpacelClick = () => {
-        if (text.length > 0) {
         let newText = text.split(/[ ]+/);
         setText(newText.join(' '));
-        showAlert("Removed Extra spaces",'Success');}
-        else{
-            showAlert("Enter some text first", "Error")
-        }
+        showAlert("Removed Extra spaces",'Success');
     }
     // Clear textbox
     const handleClearClick = () => {
-        if (text.length > 0) {
         setText('');
-        showAlert("Textbox Cleared",'Success');}
-        else{
-            showAlert("Enter some text first", "Error")
-        }
+        showAlert("Textbox Cleared",'Success');
     };
 
     // Copy text
     const handleCopyClick = () => {
-         if (text.length > 0) {
         navigator.clipboard.writeText(text);
-        showAlert("Text Copied",'Success');}
-        else{
-            showAlert("Enter some text first", "Error")
-        }
+        showAlert("Text Copied",'Success');
     };
 
     // Handle checkbox change
@@ -129,14 +96,14 @@ export default function TextForm({ heading, mode, showAlert, btnColor }) {
                         <span>Count Space, Enter and Tab as Characters</span>
                     </label>
                 </div>
-                <button type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleFirstClick}>Convert to Firstcase</button>
-                <button type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleUpClick}>Convert to Uppercase</button>
-                <button type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleLoClick}>Convert to Lowercase</button>
-                <button type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handlePascalClick}>Convert to PascalCase</button>
-                <button type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleCamellClick}>Convert to CamelCase</button>
-                <button type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleExtraSpacelClick}>Remove Extra Spaces</button>
-                <button type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleClearClick}>Clear text</button>
-                <button type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleCopyClick}>Copy text</button>
+                <button disabled={text.length===0} type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleFirstClick}>Convert to Firstcase</button>
+                <button disabled={text.length===0} type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleUpClick}>Convert to Uppercase</button>
+                <button disabled={text.length===0} type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleLoClick}>Convert to Lowercase</button>
+                <button disabled={text.length===0} type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handlePascalClick}>Convert to PascalCase</button>
+                <button disabled={text.length===0} type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleCamellClick}>Convert to CamelCase</button>
+                <button disabled={text.length===0} type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleExtraSpacelClick}>Remove Extra Spaces</button>
+                <button disabled={text.length===0} type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleClearClick}>Clear text</button>
+                <button disabled={text.length===0} type="button" className={`btn btn-${mode==='dark'? btnColor:'primary'} my-1 mx-1`} onClick={handleCopyClick}>Copy text</button>
             </div>
             <div className="container my-2" style={{ color: mode === 'dark' ? 'white' : 'black' }}>
                 <h2 className='my-2'>Your text summary</h2>
