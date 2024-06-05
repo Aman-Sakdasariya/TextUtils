ab3d2909e785aece8f9853321320c4c05a5d388b
mrams07 <amansakdasariya@gmail.com>
2024-06-05 11:36:35 +0530

    Update Nevbar.js

--- src/components/Nevbar.js
+++ src/components/Nevbar.js
@@ -6,14 +6,14 @@ export default function Nevbar({ title = "Enter Name here...", aboutText = "Ente
         <div>
             <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="#"><i><b>{title}</b></i></Link>
+                    <Link className="navbar-brand" to="/"><i><b>{title}</b></i></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" to="#">Home</Link>
+                                <Link className="nav-link active" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link" to="/About">{aboutText}</Link>
